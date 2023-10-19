4decf7b1a8b8f9fb13c8d2c8f9ea55c0810fb019
Dmitriy Korn <d.kornaukhov@tsekh.design>
2023-10-19 15:18:06 +0400

    Add some fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Research } from "./components/Research/Research";
 import { WorkLifeBalance } from "./components/WorkLifeBalance/WorkLifeBalance";
 import { Protection } from "./components/Protection/Protection";
 import { Spam } from "./components/Spam/Spam";
-import { AdvertisementBlock } from "./components/AdvertisementBlock/AdvertisementBlock";
+// import { AdvertisementBlock } from "./components/AdvertisementBlock/AdvertisementBlock";
 
 function App() {
     return (
@@ -13,7 +13,7 @@ function App() {
             <WorkLifeBalance />
             <Protection />
             <Spam />
-            <AdvertisementBlock />
+            {/*<AdvertisementBlock />*/}
         </div>
     );
 }
