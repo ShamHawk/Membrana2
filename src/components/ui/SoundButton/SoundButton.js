c1aa82f9d79a98f60b90741a1908d3b11068af82
Dmitriy Korn <d.kornaukhov@tsekh.design>
2023-10-20 17:41:18 +0400

    Add text for button and some fix

--- src/components/ui/SoundButton/SoundButton.js
+++ src/components/ui/SoundButton/SoundButton.js
@@ -1,20 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Styles from './SoundButton.module.scss';
 
 function SoundButton() {
-  const [isSoundOn, setIsSoundOn] = useState(true);
+  const [isSoundOn, setIsSoundOn] = useState(false);
+  const [showText, setShowText] = useState(true);
+
+  useEffect(() => {
+    const soundIsOn = localStorage.getItem('soundIsOn');
+    if (soundIsOn !== null) {
+      setIsSoundOn(soundIsOn === 'true');
+    }
+  }, []);
 
   const toggleSound = () => {
     setIsSoundOn((prev) => !prev);
+    setShowText(false);
   };
 
   return (
-    <div className={Styles.Btn} onClick={toggleSound}>
-      <div className={Styles.BtnWrapper}>
-        <div className={Styles.BtnContent}>
-          <img src={isSoundOn ? '/sound/sound-on.svg' : '/sound/sound-off.svg'} alt={isSoundOn ? 'Sound On' : 'Sound Off'} />
+    <div className={Styles.BtnWrapper}>
+      {showText && <div className={Styles.BtnText}>{isSoundOn ? '' : 'Включите звук для полного погружения'}</div>}
+      <div className={Styles.Btn} onClick={toggleSound}>
+        <div className={Styles.BtnInner}>
+          <div className={Styles.BtnContent}>
+            <img src={isSoundOn ? '/sound/sound-on.svg' : '/sound/sound-off.svg'} alt={isSoundOn ? 'Sound On' : 'Sound Off'} />
+          </div>
+          <div className={Styles.BtnGradient}></div>
         </div>
-        <div className={Styles.BtnGradient}></div>
       </div>
     </div>
   );
