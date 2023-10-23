f306a860a981ba36a9087c60755d011ce250c7b4
Dmitriy Korn <d.kornaukhov@tsekh.design>
2023-10-23 11:42:06 +0400

    Add styles and links for gooter block

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,20 +5,31 @@ import Styles from './Footer.module.scss';
 
 const BlockContent = () => {
   return (
-    <Container>
-      <div className={Styles.Block}>
-        <div className={Styles.Footer}>
+    <div className={Styles.Footer}>
+      <Container>
+        <div className={Styles.Wrapper}>
           <div className={Styles.LeftLink}>
-            <div>Политика обработки файлов cookie</div>
-            <div>Политика обработки персональных данных</div>
+            <a
+              href={
+                'https://moskva.mts.ru/about/investoram-i-akcioneram/korporativnoe-upravlenie/dokumenti-pao-mts/politika-obrabotka-personalnih-dannih-v-pao-mts'
+              }
+              target={'_blank'}
+            >
+              Политика обработки файлов cookie
+            </a>
+            <a href={'https://futurecrew.ru/soglasie_na_obrabotky_personalnyh_dannyh.pdf'} target={'_blank'}>
+              Политика обработки персональных данных
+            </a>
           </div>
           <div className={Styles.RightLink}>
-            <div>Продукт разработан командой FutureCrew</div>
-            <div>© 2023. Все права защищены</div>
+            <a href={'https://futurecrew.ru/'} target={'_blank'}>
+              Продукт разработан командой FutureCrew
+            </a>
+            <p>© 2023. Все права защищены</p>
           </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
