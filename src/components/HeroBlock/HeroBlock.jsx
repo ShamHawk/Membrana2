d260674b43fb2a2e205d477d5f1189149279d504
Dmitriy Korn <d.kornaukhov@tsekh.design>
2023-10-23 15:55:06 +0400

    Add adaptive for first screen

--- src/components/HeroBlock/HeroBlock.jsx
+++ src/components/HeroBlock/HeroBlock.jsx
@@ -1,17 +1,31 @@
-import Styles from './HeroBlock.module.scss';
 import { Header } from '../Header/Header';
+import { useWindowSize } from '../../hooks/useWindowSize';
+
+import Styles from './HeroBlock.module.scss';
+import Container from '../ui/Container/Container';
 
 const HeroBlock = () => {
+  const { smallMobileWidth } = useWindowSize();
+
   return (
     <div className={Styles.Block}>
       <Header />
-      <video autoPlay={true} loop muted className={Styles.Video}>
-        <source src='/video/hero-video.mp4' type='video/mp4'></source>
-      </video>
-      <p className={Styles.SubTitle}>
-        Премиум-связь
-        <br /> и сервис для бизнеса
-      </p>
+      {!smallMobileWidth && (
+        <video autoPlay={true} loop muted className={Styles.Video}>
+          <source src='/video/hero-video.mp4' type='video/mp4'></source>
+        </video>
+      )}
+      <Container>
+        {smallMobileWidth && (
+          <div className={Styles.Wrapper}>
+            <img src={'/Membrana.svg'} alt={'Membrana'} />
+          </div>
+        )}
+        <p className={Styles.SubTitle}>
+          Премиум-связь
+          <br /> и сервис для бизнеса
+        </p>
+      </Container>
     </div>
   );
 };
