93d2ff0b345d24354fc85f6c1ac3c2f70f2bbbe5
Dmitriy Korn <d.kornaukhov@tsekh.design>
2023-10-27 17:28:47 +0400

    Add some fix

--- src/components/FormBlock/FormBlock.jsx
+++ src/components/FormBlock/FormBlock.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Container from '@components/ui/Container/Container';
 
 import Styles from './FormBlock.module.scss';
-import classNames from "classnames";
+import classNames from 'classnames';
 
 const BlockContent = ({ formBlockRef }) => {
   useEffect(() => {
@@ -17,51 +17,55 @@ const BlockContent = ({ formBlockRef }) => {
   }, []);
 
   return (
-      <div className={Styles.Block} ref={formBlockRef}>
-        <div className={classNames(Styles.Title, Styles.Title_one)}>
-          <h2>Ваши данные —<br/> это ваша собственность</h2>
-        </div>
-        <div className={classNames(Styles.Subtitle, Styles.Subtitle_one)}>
-          <p>Мы гарантируем, что они не используются для монетизации и не передаются третьим лицам.</p>
-        </div>
-
-
-        <div className={Styles.HeadBlock}>
-          <div className={classNames(Styles.Title, Styles.Title_two)}>
-            <h2>Позаботьтесь о команде</h2>
-          </div>
-          <div className={classNames(Styles.Subtitle, Styles.Subtitle_twoz)}>
-            <p>Заполните контактную информацию, мы свяжемся с вами, расскажем про продукт, покажем Демо.</p>
-          </div>
-        </div>
-        {/*<div className={Styles.Form}>*/}
-        {/*    <div className={Styles.FormContainer}>*/}
-        {/*        <input type={"text"} placeholder={'Контактное лицо*'} className={Styles.FormInput}/>*/}
-        {/*    </div>*/}
-        {/*    <div className={Styles.FormContainer}>*/}
-        {/*        <input type={"text"} placeholder={'Номер для связи*'} className={Styles.FormInput}/>*/}
-        {/*    </div>*/}
-        {/*    <div className={Styles.FormContainer}>*/}
-        {/*        <input type={"text"} placeholder={'Название компании или ИНН*'} className={Styles.FormInput}/>*/}
-        {/*    </div>*/}
-        {/*    <div className={Styles.FormContainer}>*/}
-        {/*        <input type={"text"} placeholder={'Количество сотрудников*'} className={Styles.FormInput}/>*/}
-        {/*    </div>*/}
-        {/*    <div className={Styles.FormContainer}>*/}
-        {/*        <input type={"text"} placeholder={'Комментарий'} className={Styles.FormInput}/>*/}
-        {/*    </div>*/}
-        {/*</div>*/}
-        <div className='form-container'>
-          <div
-              id='digital_highway'
-              data-bo-theme={"dark"}
-              data-product='PL0146'
-              // data-bo-custom-styles="tb"
-              data-vitrine='membrana'
-              data-region='77'/>
-        </div>
+    <div className={Styles.Block} ref={formBlockRef}>
+      <div className={classNames(Styles.Title, Styles.Title_one)}>
+        <h2>
+          Ваши данные —<br /> это ваша собственность
+        </h2>
+      </div>
+      <div className={classNames(Styles.Subtitle, Styles.Subtitle_one)}>
+        <p>Мы гарантируем, что они не используются для монетизации и не передаются третьим лицам.</p>
       </div>
 
+      <div className={Styles.HeadBlock}>
+        <div className={classNames(Styles.Title, Styles.Title_two)}>
+          <h2>Позаботьтесь о команде</h2>
+        </div>
+        <div className={classNames(Styles.Subtitle, Styles.Subtitle_twoz)}>
+          <p>Заполните контактную информацию, мы свяжемся с вами, расскажем про продукт, покажем Демо.</p>
+        </div>
+      </div>
+      {/*<div className={Styles.Form}>*/}
+      {/*    <div className={Styles.FormContainer}>*/}
+      {/*        <input type={"text"} placeholder={'Контактное лицо*'} className={Styles.FormInput}/>*/}
+      {/*    </div>*/}
+      {/*    <div className={Styles.FormContainer}>*/}
+      {/*        <input type={"text"} placeholder={'Номер для связи*'} className={Styles.FormInput}/>*/}
+      {/*    </div>*/}
+      {/*    <div className={Styles.FormContainer}>*/}
+      {/*        <input type={"text"} placeholder={'Название компании или ИНН*'} className={Styles.FormInput}/>*/}
+      {/*    </div>*/}
+      {/*    <div className={Styles.FormContainer}>*/}
+      {/*        <input type={"text"} placeholder={'Количество сотрудников*'} className={Styles.FormInput}/>*/}
+      {/*    </div>*/}
+      {/*    <div className={Styles.FormContainer}>*/}
+      {/*        <input type={"text"} placeholder={'Комментарий'} className={Styles.FormInput}/>*/}
+      {/*    </div>*/}
+      {/*</div>*/}
+      <div className={Styles.FormWrapper}>
+        <div className={Styles.FormBlur}></div>
+        <div className='form-container'>
+          <div
+            id='digital_highway'
+            data-bo-theme={'dark'}
+            data-product='PL0146'
+            // data-bo-custom-styles="tb"
+            data-vitrine='membrana'
+            data-region='77'
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
