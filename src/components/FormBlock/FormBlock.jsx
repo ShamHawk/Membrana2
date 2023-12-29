1189a6f77b4e447e19efac3ec52bbe2488dc0854
Danil Troshin <troshin@MacBook-Air-Danil.local>
2023-12-29 05:32:25 +0400

    правки

--- src/components/FormBlock/FormBlock.jsx
+++ src/components/FormBlock/FormBlock.jsx
@@ -1,15 +1,15 @@
+import React, {useEffect, useState} from "react";
 import { useWindowSize } from '../../hooks/useWindowSize';
 import * as yup from 'yup';
-import InputMask from 'react-input-mask';
+
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 
 import Styles from './FormBlock.module.scss';
 import classNames from "classnames";
-import React, {useEffect, useState} from "react";
-import CheckBox from "../ui/CheckBox/CheckBox";
-import Ticker from "../ui/Ticker/Ticker";
+
+
 import axios from "axios";
 
 const BlockContent = ({ formBlockRef }) => {
@@ -23,8 +23,6 @@ const BlockContent = ({ formBlockRef }) => {
         },
         withCredentials: false,
     });
-
-
     const cleanPhoneNumber = (value) => {
         const cleanedValue = value.replace(/[\s()-]/g, '');
         return cleanedValue;
@@ -113,8 +111,6 @@ const test =  {
         mode: 'onSubmit',
         resolver: yupResolver(schemaForm),
     });
-
-
   return (
       <div className={Styles.Block} >
         <div className={classNames(Styles.Title, Styles.Title_one)}>
@@ -143,7 +139,6 @@ const test =  {
               {/*</p>*/}
           </div>
         </div>
-
           <div className={Styles.FormWrapper} >
               {/*<form className={Styles.formBlock}  onSubmit={handleSubmit(formSubmit)}>*/}
               {/*    <div className={Styles.Form}>*/}
@@ -197,6 +192,39 @@ const test =  {
               {/*</form>*/}
               {/*<div className={Styles.FormSendsey} data-sendsay-form-embedded="x_1702990490671778/1"></div>*/}
               <div  className={Styles.FormSendsey} data-sendsay-form-embedded="mts_sendmail/6"></div>
+              {/*<div className={Styles.testicks}>*/}
+              {/*    <form className={Styles.newForm}  action="https://sendsay.ru/form/mts_sendmail/6/" method="post">*/}
+              {/*        <label className={Styles.newForm__labelInput}>*/}
+              {/*            <input placeholder={"Email*"} className={Styles.newForm__input} name="_member_email"/>*/}
+              {/*        </label>*/}
+              {/*        <label className={Styles.newForm__labelInput}>*/}
+              {/*            <input placeholder={"Контактное лицо*"} className={Styles.newForm__input} type="text" name="q871" value="" size="256"/>*/}
+              {/*        </label>*/}
+              {/*        <label className={Styles.newForm__labelInput}>*/}
+              {/*            <input placeholder={"Номер для связи*"} className={Styles.newForm__input} type="text" name="q24" value="" size="100"/>*/}
+              {/*        </label>*/}
+              {/*        <label >*/}
+              {/*            <label className={Styles.newForm__label} style={{display:'flex'}}>*/}
+              {/*                <div>*/}
+              {/*                    <input className={Styles.newForm__checkBox} type="checkbox" name="q872" value="1"  />*/}
+              {/*                </div>*/}
+              {/*                Я выражаю своё согласие на обработку персональных данных, а также подтверждаю факт ознакомления с Политикой обработки персональных данных*/}
+              {/*            </label>*/}
+              {/*        </label>*/}
+              {/*        <label>*/}
+              {/*            <label className={Styles.newForm__label} style={{display:'flex'}}>*/}
+              {/*                <div>*/}
+              {/*                    <input className={Styles.newForm__checkBox} type="checkbox" name="q148" value="1"  />*/}
+              {/*                </div>*/}
+              {/*                Даю согласие на использование этой почты для связи со мной. Это простая и при этом важная формальность ? подтвердите своё согласие, чтобы мы могли вам писать.*/}
+              {/*            </label>*/}
+              {/*        </label>*/}
+              {/*        <label>*/}
+              {/*            <input className={Styles.newForm__inputNone} type="text" name="q672" value="Membrana.me" size="100"/>*/}
+              {/*        </label>*/}
+              {/*        <input className={Styles.newForm__button} type="submit" value="отправить"/>*/}
+              {/*    </form>*/}
+              {/*</div>*/}
               {/*<div className={Styles.FormSendsey} data-sendsay-form-embedded="x_1702990490671778/1"></div>*/}
       </div>
     </div>
